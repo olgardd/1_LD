ebe98683402f6091937f3ef9120da492a5b3687a
20162104 <36658778+olgardd@users.noreply.github.com>
2018-02-20 10:48:06 +0200

    ikeliami pirminiai failai

---
+++ ca-master/xor.vhd
@@ -0,0 +1,16 @@
+-- File: xor.vhd
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- the entity
+entity xor is
+port (
+i_a, i_b : in std_logic;
+o_f : out std_logic);
+end xor;
+
+-- the architecture
+architecture xor_arc of xor is
+begin
+o_f <= i_a xor i_b;
+end xor_arc;
